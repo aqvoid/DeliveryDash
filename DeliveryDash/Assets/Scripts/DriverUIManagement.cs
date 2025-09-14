d0a5633e21246e7d17a957ca0cfff2be67a286d9
aqvoid <mindeow35@gmail.com>
2025-09-14 23:03:36 +0300

    Added Packages Counter
    - added text how much packages are left on scene

--- DeliveryDash/Assets/Scripts/DriverUIManagement.cs
+++ DeliveryDash/Assets/Scripts/DriverUIManagement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class DriverUIManagement : MonoBehaviour
 {
     internal static TMP_Text boostText;
+    
+    private int currentPackages;
+    private TMP_Text packagesText;
 
     [Header("UI")]
     [SerializeField] private GameObject canvasGameObject;
@@ -11,8 +14,14 @@ public class DriverUIManagement : MonoBehaviour
 
     void Awake()
     {
-        boostText = canvasGameObject.GetComponentInChildren<TMP_Text>();
+        packagesText = GameObject.Find("Packages Text").GetComponent<TMP_Text>();
+        boostText = GameObject.Find("Boost Text").GetComponent<TMP_Text>();
         boostText.gameObject.SetActive(false);
     }
-    
+
+    private void Update()
+    {
+        currentPackages = GameObject.FindGameObjectsWithTag("Customer").Length;
+        packagesText.text = $"Packages left: {currentPackages}";
+    }
 }
