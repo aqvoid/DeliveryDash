0ac1f90799647eb198c1f51ca266c1a6a7b19b45
aqvoidDev <114750046+aqvoid@users.noreply.github.com>
2025-09-16 19:52:26 +0300

    Fixing conflict between branches
    - refactoring and reorganizing a few UI scripts

--- DeliveryDash/Assets/Scripts/DriverUIManagement.cs
+++ DeliveryDash/Assets/Scripts/DriverUIManagement.cs
@@ -4,46 +4,41 @@ using UnityEngine.SceneManagement;
 
 public class DriverUIManagement : MonoBehaviour
 {
-    private int currentPackages;
 
-    [Header("UI")]
-    [SerializeField] internal TMP_Text boostText;
-    [SerializeField] private GameObject victoryWindow;
+    [Header("=== Packages ===")]
+    [SerializeField] private PackagesController packagesScript;
     [SerializeField] private TMP_Text packagesText;
 
-    [SerializeField] private Stopwatch stopwatchScript;
+    [Header("=== Boost ===")]
+    [SerializeField] private BoostController boostScript;
+    [SerializeField] private TMP_Text boostText;
+
+    [Header("=== Victory Window ===")]
+    [SerializeField] private VictoryWindowController victoryWindow;
+
+    [Header("=== Stopwatch ===")]
+
+    [SerializeField] private StopwatchController stopwatchScript;
     [SerializeField] private TMP_Text stopwatchText;
 
 
     void Awake()
     {
         ToggleBoostText(false);
-
-        packagesText = GameObject.Find("Packages Text").GetComponent<TMP_Text>();
-        boostText = GameObject.Find("Boost Text").GetComponent<TMP_Text>();
-        boostText.gameObject.SetActive(false);
     }
     
     private void Update()
     {
         ChangeStopwatchText(stopwatchScript.ElapsedTime);
+        ChangePackagesText(packagesScript.CurrentPackages);
 
-        currentPackages = GameObject.FindGameObjectsWithTag("Customer").Length;
-        packagesText.text = $"Packages left: {currentPackages}";
         ToggleVictoryWindow();
     }
-
-    internal void ToggleBoostText(bool toggleText) => boostText.gameObject.SetActive(toggleText);
-
     private void ToggleVictoryWindow()
     {
-        victoryWindow.SetActive(currentPackages > 0 ? false : true);
-        Time.timeScale = victoryWindow.activeInHierarchy ? 0 : 1;
+        victoryWindow.gameObject.SetActive(packagesScript.CurrentPackages > 0 ? false : true);
+        Time.timeScale = victoryWindow.gameObject.activeInHierarchy ? 0 : 1;
     }
-
-    public void RestartButton() => SceneManager.LoadScene(0);
-    public void QuitButton() => Application.Quit();
-
     private void ChangeStopwatchText(float time)
     {
         int minutes = Mathf.FloorToInt(time / 60f);
@@ -52,4 +47,16 @@ public class DriverUIManagement : MonoBehaviour
 
         stopwatchText.text = "Time passed: " + string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds);
     }
+
+    private void ChangePackagesText(int packagesCount)
+    {
+        packagesText.text = $"Packages left: {packagesCount}";
+    }
+    
+    public void ToggleBoostText(bool state) => boostText.gameObject.SetActive(state);
+    public void RestartButton() => SceneManager.LoadScene(0);
+    public void QuitButton() => Application.Quit();
+
+    public void ShowVictory() => victoryWindow.Show();
+    public void HideVictory() => victoryWindow.Hide();
 }
