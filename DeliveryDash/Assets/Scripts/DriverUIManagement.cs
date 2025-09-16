673f476fc6c6d092e3c670866e592a89c9a90e3e
aqvoidDev <114750046+aqvoid@users.noreply.github.com>
2025-09-16 18:05:41 +0300

    Added Stopwatch to UI

--- DeliveryDash/Assets/Scripts/DriverUIManagement.cs
+++ DeliveryDash/Assets/Scripts/DriverUIManagement.cs
@@ -8,20 +8,30 @@ public class DriverUIManagement : MonoBehaviour
     private int currentPackages;
     private TMP_Text packagesText;
 
-    [Header("UI")]
-    [SerializeField] private GameObject canvasGameObject;
-
+    [SerializeField] private Stopwatch stopwatchScript;
+    [SerializeField] private TMP_Text stopwatchText;
 
     void Awake()
     {
+
         packagesText = GameObject.Find("Packages Text").GetComponent<TMP_Text>();
         boostText = GameObject.Find("Boost Text").GetComponent<TMP_Text>();
         boostText.gameObject.SetActive(false);
     }
-
     private void Update()
     {
+        ChangeStopwatchText(stopwatchScript.ElapsedTime);
+
         currentPackages = GameObject.FindGameObjectsWithTag("Customer").Length;
         packagesText.text = $"Packages left: {currentPackages}";
+    }
+
+    private void ChangeStopwatchText(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time % 60f);
+        int milliseconds = Mathf.FloorToInt((time * 100) % 100);
+
+        stopwatchText.text = "Time passed: " + string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds);
     }
 }
